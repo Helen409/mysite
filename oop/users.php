6f79dd19944c9e5d270c3f276420ffb910573388
Helen Sakovych <helen40931@gmail.com>
2018-04-26 11:35:41 +0300

    26.04.18

--- oop/users.php
+++ oop/users.php
@@ -27,6 +27,20 @@ class User{
 		$this->password='';
 	}
 }
+class SuperUser extends User{
+	public $role;
+	public function showInfo(){
+		parent::ShowInfo();
+		echo 'Роль: ',$this->role;
+		$this->myHr();
+	}
+	public function __construct($name,$login,$password,$role){
+		parent::__construct($name,$login,$password);
+		$this->role=$role;
+		
+	}
+}
+$user=new SuperUser('Администратор','admin','qwerty','admin');
 $user1=new User('Вася','vasya23','123456');
 $user2=new User('Света','Sveta','123456');
 $user3=new User('Таня','tanushka','123456');
@@ -36,4 +50,5 @@ echo $user1->showInfo();
 echo $user2->showInfo();
 echo $user3->showInfo();
 echo $user4->showInfo();
+echo $user->showInfo();
 ?> 
